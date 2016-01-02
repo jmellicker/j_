223477cec96a9a3bda144954b28097578ca87f31
jmellicker <jmellicker@users.noreply.github.com>
2016-01-01 17:47:02 -0800

    Update j_.js

--- j_.js
+++ j_.js
@@ -30,6 +30,9 @@ var j_ = {
         else if (string.indexOf('/') > -1) {
             return '/'
         }
+        else if (string.indexOf('|') > -1) {
+            return '|'
+        }
         else {
             return "error: no delimiter"
         }
