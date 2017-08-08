02ae02fa3afdccfe59f998824d339d160c4fc2a4
Daniel Wolner <daniel.wolner@gmail.com>
2017-08-07 17:40:24 -0700

    added luhnCheck

--- index.js
+++ index.js
@@ -737,5 +737,28 @@ module.exports = {
             }
         }
         return b[i]? -1:0;
+    },
+    
+    luhnCheck: function(value) {
+      // accept only digits, dashes or spaces
+        if (/[^0-9-\s]+/.test(value)) return false;
+
+        // The Luhn Algorithm. It's so pretty.
+        var nCheck = 0, nDigit = 0, bEven = false;
+        value = value.replace(/\D/g, "");
+
+        for (var n = value.length - 1; n >= 0; n--) {
+            var cDigit = value.charAt(n),
+                  nDigit = parseInt(cDigit, 10);
+
+            if (bEven) {
+                if ((nDigit *= 2) > 9) nDigit -= 9;
+            }
+
+            nCheck += nDigit;
+            bEven = !bEven;
+        }
+
+        return (nCheck % 10) == 0;
     }
 }
